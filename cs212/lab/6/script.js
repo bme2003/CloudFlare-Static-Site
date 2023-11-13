04d3649cb70eb9187600db29dba2fc27b57a2b8d
bme2003 <123276917+bme2003@users.noreply.github.com>
2023-11-13 13:41:43 -0700

    Update script.js

--- cs212/lab/6/script.js
+++ cs212/lab/6/script.js
@@ -1,19 +1,20 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const randomNumber = Math.floor(Math.random() * 100) + 1;
-    let guesses = 0;
-
-    document.querySelector('#guessButton').addEventListener('click', () => {
-        const userGuess = parseInt(document.querySelector('#guessInput').value);
-        guesses++;
-        let result = '';
-        if (userGuess === randomNumber) {
-            result = `Correct! It took you ${guesses} guesses.`;
-        } else if (userGuess < randomNumber) {
-            result = 'Too low!';
+function displayGreeting() {
+    var inputText = document.getElementById("input-text").value;
+    var translatedText = translateToPigLatin(inputText);
+    var greetingMessage = "Hello, here is that translation, " + translatedText + ". Welcome to our interactive page!";
+    document.getElementById("piglatin-message").innerText = greetingMessage;
+}
+function translateToPigLatin(text) {
+    return text.split(' ').map(word => {
+        // Check if the first character is a vowel
+        if (/[aeiouAEIOU]/.test(word[0])) {
+            return word + "way";
         } else {
-            result = 'Too high!';
+            // Find the index of the first vowel
+            const index = word.search(/[aeiouAEIOU]/);
+            // If no vowel is found, return the original word
+            if (index === -1) return word;
+            // Move all characters before the first vowel to the end and add "ay"
+            return word.substring(index) + word.substring(0, index) + "ay";
         }
-        document.querySelector('#result').textContent = result;
-    });
-});
-
+    }).join(' ');
